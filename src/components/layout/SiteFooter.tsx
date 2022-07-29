9fcb70dc8d5a9ca3a373da2207c8bf9148a322c1
Alexander Six <alexanderhsix@gmail.com>
2022-07-28 21:09:20 -0400

    Adds site footer

---
+++ src/components/layout/SiteFooter.tsx
@@ -0,0 +1,8 @@
+export const SiteFooter = () => {
+    return (
+        <footer class="flex flex-col items-center justify-center p-6 bg-black-dark">
+            <hr class="site-gradient h-0.5 w-8 flex-grow rounded-lg border-none mb-4" />
+            <span class="text-white-alt text-xs font-light">Made with ☕️ by Alexander Six – © 2022</span>
+        </footer>
+    );
+};
