837cfa4efec37bf5121b83f2a286370a7958d7eb
Alexander Six <alexanderhsix@gmail.com>
2022-08-01 23:30:08 -0400

    WIP

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,12 @@
 import { defineConfig } from "astro/config";
 import preact from "@astrojs/preact";
-
 import tailwind from "@astrojs/tailwind";
-
 import shiki from "shiki";
-
+// import image from "@astrojs/image";
+import mdx from "@astrojs/mdx";
 const gruvboxUrl = new URL("./src/data/gruvbox.json", import.meta.url).pathname;
 const gruvbox = await shiki.loadTheme(gruvboxUrl);
+
 
 // https://astro.build/config
 export default defineConfig({
@@ -14,7 +14,7 @@ export default defineConfig({
   integrations: [preact(), tailwind()],
   markdown: {
     shikiConfig: {
-      theme: gruvbox,
-    },
-  },
+      theme: gruvbox
+    }
+  }
 });
